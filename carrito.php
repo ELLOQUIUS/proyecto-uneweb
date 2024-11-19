417b501cba56cc6c5c3898ad242af6a92cc92913
ELLOQUIUS <elloquius@gmail.com>
2024-11-18 23:04:30 -0400

    Barra de navegacion,index y estilos basicos

---
+++ carrito.php
@@ -0,0 +1,5 @@
+<?php
+    include "./navegacion.php";
+    include "./heading.php";
+?>
+<h1>Hola<h1>
---
+++ navegacion.php
@@ -0,0 +1,11 @@
+<?php
+    include "./heading.php";
+?>
+<div class="navegacion">
+    <h2 class="nombre-carrito">Carrito Miguel</h2>
+    <div class="contenedor-enlaces">
+        <a class="enlace" href="index.php">Home</a>
+        <a class="enlace" href="carrito.php">Carrito</a>
+    </div>  
+    <button class="inicio-sesion">Iniciar Sesión</button>
+</div>
