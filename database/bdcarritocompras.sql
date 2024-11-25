b376c4fdbd6af3dfdfb7313913fbb2b4ee069ad3
ELLOQUIUS <elloquius@gmail.com>
2024-11-25 00:29:18 -0400

    Se actualiza la bd para que la tabla cliente tenga el atributo administrador, junto con algunos inserts

--- database/bdcarritocompras.sql
+++ database/bdcarritocompras.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-11-2024 a las 02:37:21
+-- Tiempo de generación: 25-11-2024 a las 05:26:18
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -33,8 +33,31 @@ CREATE TABLE `cliente` (
   `nombre` varchar(255) NOT NULL,
   `direccion` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `contrasena` varchar(50) NOT NULL
+  `contrasena` varchar(50) NOT NULL,
+  `administrador` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`idCliente`, `dni`, `nombre`, `direccion`, `email`, `contrasena`, `administrador`) VALUES
+(3, '9095692', 'miguel', 'mi casa', 'elloquius@gmail.com', '123', 0),
+(4, '28416652', 'luis', 'la casa', 'rogerwtf4+naz9@gmail.com', '123', 0),
+(5, '28416652', 'fredy', 'la casa', 'rogerwtf4+naz9@gmail.com', '123', 0),
+(6, '29861038', 'lokas', 'la casa', 'elloquius@gmail.com', '444', 0),
+(7, '9095692', 'miguelllll', 'la casa', 'elloquius@gmail.com', '333', 0),
+(8, '9095692', 'salo', 'la casa', 'elloquius@gmail.com', '555', 0),
+(9, '29861038', 'sapo', 'qqweqwe', 'rogerwtf4+naz9@gmail.com', '123', 0),
+(10, '1213', 'carla', 'la casa', 'rogerwtf4+naz9@gmail.com', '23', 1),
+(11, '28416652', 'carlos', 'la casa', 'rogerwtf4+naz9@gmail.com', '123', 1),
+(12, '123123', 'sheo', 'si', 'esto@gmail.com', '111', 0),
+(13, '123123', 'ki', '232323', 'sss@gmail.com', '22', 0),
+(14, '123123', 'asdasda', 'asdasd', 'wqeqwe', '123123', 1),
+(15, '9095692', 'pedro', 'asdasd', 'elloquius@gmail.com', '22', 1),
+(16, '9095692', 'shs', 'la casa', 'rogerwtf4+naz9@gmail.com', '222', 0),
+(17, '28416652', 'zain', 'mi piso', 'migueluchogordillo@gmail.com', '123', 0),
+(18, '28416652', 'lala', 'qqweqwe', 'rogerwtf4+naz9@gmail.com', '333', 1);
 
 -- --------------------------------------------------------
 
@@ -45,8 +68,8 @@ CREATE TABLE `cliente` (
 CREATE TABLE `compras` (
   `idCompras` int(11) NOT NULL,
   `idCliente` int(11) NOT NULL,
-  `FechaCompra` varchar(11) NOT NULL,
-  `Monto` double NOT NULL
+  `fechaCompra` varchar(11) NOT NULL,
+  `monto` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -59,8 +82,8 @@ CREATE TABLE `detalle_compras` (
   `idDetalle` int(11) NOT NULL,
   `idProducto` int(11) NOT NULL,
   `idCompras` int(11) NOT NULL,
-  `Cantidad` int(11) NOT NULL,
-  `PrecioCompra` double NOT NULL
+  `cantidad` int(11) NOT NULL,
+  `precioCompra` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -72,11 +95,22 @@ CREATE TABLE `detalle_compras` (
 CREATE TABLE `producto` (
   `idProducto` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
-  `foto` varchar(255) DEFAULT NULL,
+  `foto` varchar(512) DEFAULT NULL,
   `descripcion` text DEFAULT NULL,
   `precio` double NOT NULL,
   `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idProducto`, `nombre`, `foto`, `descripcion`, `precio`, `stock`) VALUES
+(1, 'Iphone 8', 'https://m.media-amazon.com/images/I/41+CLiWu9lL.jpg', 'Celular de alta calidad, muy bueno para fotos', 188, 4),
+(2, 'Smart TV', 'https://images-na.ssl-images-amazon.com/images/I/81R3dLptKcL._AC_UL600_SR600,400_.jpg', 'Increible resolución, con gráficos 4k', 199, 2),
+(3, 'Chromecast', 'https://m.media-amazon.com/images/I/510wm50VDHL._AC_SX425_.jpg', 'Control y adaptador chromecast con gráficos 4k', 99, 10),
+(4, 'Cámara Go Pro', 'https://http2.mlstatic.com/D_NQ_NP_733488-MLV74339217428_022024-O.webp', 'Una cámara para grabar tus mejores aventuras y acrobacias', 32.57, 12),
+(5, 'wii', 'https://www.nintendo.com/eu/media/images/10_share_images/support_9/H2x1_NintendoWii_support_no_logo_image1280w.jpg', 'La mejor consola de 2012', 100, 5);
 
 --
 -- Índices para tablas volcadas
@@ -117,7 +151,7 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `compras`
@@ -135,7 +169,7 @@ ALTER TABLE `detalle_compras`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
